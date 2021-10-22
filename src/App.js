3844de08d8ba42995d8012e772960de19534540a
Drakies <58969005+Drakies3172@users.noreply.github.com>
2021-10-22 16:39:59 +0700

    start branching

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
-import './App.css';
-
-//import 
-import themes from './theme'
-import { useSelector } from 'react-redux';
-import { BrowserRouter, Route, Switch} from "react-router-dom";
-
-//import library
-import { ThemeProvider } from '@material-ui/core/styles';
-
-//import project
-import HomePage from './pages/HomePage/HomePage'
-import PageNotFound from './pages/PageNotFound/PageNotFound'
-
+import themes from "./theme";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { ThemeProvider } from "@material-ui/core/styles";
+import HomePage from "./pages/HomePage/HomePage";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
 
 function App() {
   const customization = useSelector((state) => state.customization);
   return (
-    <div >
-      <ThemeProvider theme={themes(customization)} >
+    <div>
+      <ThemeProvider theme={themes(customization)}>
         <BrowserRouter>
-            <Switch>
-              {/* Fix lại route */}
-              <Route path="/" component={HomePage} exact/>
-              <Route path="/home" component={HomePage} />
-              <Route path="*" component={PageNotFound} />
-            </Switch>
-          </BrowserRouter>
+          <Switch>
+            {/* Fix lại route */}
+            <Route path="/" component={HomePage} exact />
+            <Route path="/home" component={HomePage} />
+            <Route path="*" component={PageNotFound} />
+          </Switch>
+        </BrowserRouter>
       </ThemeProvider>
     </div>
-    
-    
   );
 }
 
