070a72100c56d12ffdff812996576b69543e69c0
Hai Ly <53532432+haily835@users.noreply.github.com>
2022-01-25 19:30:24 +0700

    excel

--- src/views/InventoryView/Inventory/Inventory.js
+++ src/views/InventoryView/Inventory/Inventory.js
@@ -37,11 +37,9 @@ import ToolBar from "../../../components/TableCommon/ToolBar/ToolBar";
 import TableWrapper from "../../../components/TableCommon/TableWrapper/TableWrapper";
 import { useSelector } from "react-redux";
 
-import * as excel from '../../../assets/constant/excel'
+import * as excel from "../../../assets/constant/excel";
 
-
-
-import * as xlsx from 'xlsx'
+import * as xlsx from "xlsx";
 const Inventory = () => {
   const [productList, setProductList] = useState([]);
   const [reload, setReload] = useState(true);
@@ -50,19 +48,19 @@ const Inventory = () => {
   const store_uuid = info.store.uuid;
   const branch_uuid = info.branch.uuid;
 
-
   const importProductByJSON = async (jsonData) => {
-
     try {
-      const res = await storeApi.importProductJSON(store_uuid,jsonData);
+      console.log("loading = true");
+      const res = await storeApi.importProductJSON(store_uuid, jsonData);
       console.log(res.message);
-      setOpen(true);
+      console.log("loaidng false");
+
+      // setOpen(true);
     } catch (err) {
       console.log(err);
-      setOpen(false);
+      // setOpen(false);
     }
-  }
-
+  };
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -72,7 +70,7 @@ const Inventory = () => {
           branch_uuid
         );
         setProductList(response.data);
-        console.log(response.data)
+        console.log(response.data);
       } catch (err) {
         console.log(err);
       }
@@ -91,7 +89,7 @@ const Inventory = () => {
           branch_uuid
         );
         setProductList(response.data);
-        console.log(response.data)
+        console.log(response.data);
       } catch (err) {
         console.log(err);
       }
@@ -171,12 +169,11 @@ const Inventory = () => {
     content: () => componentRef.current,
   });
 
-
-  const [jsonData, setJson] = useState([])
-//   // import excel
-//   const importExcel = async (json) =>{
-//     importProductByJSON(json)
-//   }
+  const [jsonData, setJson] = useState([]);
+  //   // import excel
+  //   const importExcel = async (json) =>{
+  //     importProductByJSON(json)
+  //   }
 
   return (
     <Card className={classes.root}>
@@ -230,15 +227,13 @@ const Inventory = () => {
         tableType={TableType.INVENTORY}
         handlePrint={handlePrint}
         handleSearchValueChange={setSearchValue}
-
         hasImport={true}
-
         importProductByJSON={importProductByJSON}
         excel_head={excel.header_product}
         excel_data={excel.excel_data_product}
         excel_name={excel.excel_name_product}
       />
-      
+
       {/* 3. TABLE */}
       <TableWrapper>
         <TableHeader
