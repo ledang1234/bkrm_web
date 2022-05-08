966b37df83f1c044fdba40947fe89d002daaa00c
haily835 <53532432+haily835@users.noreply.github.com>
2022-05-08 07:09:20 +0700

    lazyload

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import themes from "./theme";
+import React, { Suspense } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  BrowserRouter,
   Route,
   Switch,
   Redirect,
   HashRouter,
 } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core/styles";
-import HomePage from "./pages/HomePage/HomePage";
+// import HomePage from "./pages/HomePage/HomePage";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import Customization from "./components/Customization/Customization";
-import SignupPage from "./pages/SignupPage/SignupPage";
-import { verifyToken, setCustomization, loadBranches } from "./store/actionCreator";
+// import LoginPage from "./pages/LoginPage/LoginPage";
+// import SignupPage from "./pages/SignupPage/SignupPage";
+import { verifyToken } from "./store/actionCreator";
 import { useEffect, useState } from "react";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import LoadingModal from "./components/LoadingModal/LoadingModal";
-import MainPage from "./pages/MainPage/MainPage";
-import CustomerPage from "./pages/CustomerPage/CustomerPage";
-import { Box, CssBaseline, makeStyles } from "@material-ui/core";
+// import MainPage from "./pages/MainPage/MainPage";
+// import CustomerPage from "./pages/CustomerPage/CustomerPage";
+import { Box, CssBaseline } from "@material-ui/core";
 import GlobalSnackbar from "./components/GlobalSnackBar/GlobalSnackBar";
 import Test from "./components/Category/Test";
-import { customizeAction } from "./store/slice/customizeSlice";
-import { SwitchCamera } from "@material-ui/icons";
 import "./index.css";
+
+const HomePage = React.lazy(() => import("./pages/HomePage/HomePage"));
+const LoginPage = React.lazy(() => import("./pages/LoginPage/LoginPage"));
+const SignupPage = React.lazy(() => import("./pages/SignupPage/SignupPage"));
+const CustomerPage = React.lazy(() => import("./pages/CustomerPage/CustomerPage"));
+const MainPage = React.lazy(() => import("./pages/MainPage/MainPage"));
 function App() {
   const customization = useSelector((state) => state.customize);
   const store_uuid = useSelector((state) => state.info.store.uuid);
@@ -42,6 +45,8 @@ function App() {
   // })
   return (
     <ThemeProvider theme={themes(customization)}>
+      <Suspense fallback={<LoadingModal />}>
+
       <Box>
         <LoadingModal />
         <GlobalSnackbar />
@@ -75,6 +80,7 @@ function App() {
         </HashRouter>
         {/* <Customization /> */}
       </Box>
+      </Suspense>
     </ThemeProvider>
   );
 }
